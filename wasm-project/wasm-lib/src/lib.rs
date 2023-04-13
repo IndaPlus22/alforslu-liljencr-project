a0fce0d35bbfcebdaa3377234f53968a735b9669
Duckkyy <alex@forslund.dev>
2023-04-13 14:56:30 +0200

    Implement function to calculate 2D vector of universal gravitation

--- wasm-project/wasm-lib/src/lib.rs
+++ wasm-project/wasm-lib/src/lib.rs
@@ -14,10 +14,19 @@ pub fn add(left: usize, right: usize) -> usize {
 // m2 = mass of object 2
 // r = distance between object 1 and object 2
 // Returns the force of attraction between two objects
-#[wasm_bindgen]
 pub fn get_attraction_force(m1: f64, m2: f64, r: f64) -> f64 {
     let g = 6.67408 * 10.0_f64.powi(-11); 
     g * (m1 * m2) / r.powi(2)
+}
+
+// Newton's Law of Universal Gravitation in 2D
+// r = |r2 - r1|
+// Result is the force applied on object 2 exerted by object 1
+// https://en.wikipedia.org/wiki/Newton%27s_law_of_universal_gravitation#Vector_form
+pub fn get_attraction_force_vector3(m1: f64, m2: f64, r: na::Vector2<f64>) -> na::Vector2<f64> {
+    let g = 6.67408 * 10.0_f64.powi(-11); 
+    let force = -g * (m1 * m2) / r.norm_squared(); // Force is negative because it is applied on object 2
+    r.normalize() * force // Normalize the vector and multiply by the force
 }
 
 
@@ -32,5 +41,7 @@ mod tests {
         let result = get_attraction_force(1.0, 1.0, 1.0);
         assert_eq!(result, 6.67408 * 10.0_f64.powi(-11));
     }
+
+    // Test attraction force vector function
 }
 
