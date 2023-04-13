ac702f8dfe1511290f397e95d9877d8b40bc379e
Duckkyy <alex@forslund.dev>
2023-04-13 15:05:09 +0200

    Fix function name typo

--- wasm-project/wasm-lib/src/lib.rs
+++ wasm-project/wasm-lib/src/lib.rs
@@ -23,7 +23,7 @@ pub fn get_attraction_force(m1: f64, m2: f64, r: f64) -> f64 {
 // r = |r2 - r1|
 // Result is the force applied on object 2 exerted by object 1
 // https://en.wikipedia.org/wiki/Newton%27s_law_of_universal_gravitation#Vector_form
-pub fn get_attraction_force_vector3(m1: f64, m2: f64, r: na::Vector2<f64>) -> na::Vector2<f64> {
+pub fn get_attraction_force_vector2(m1: f64, m2: f64, r: na::Vector2<f64>) -> na::Vector2<f64> {
     let g = 6.67408 * 10.0_f64.powi(-11); 
     let force = -g * (m1 * m2) / r.norm_squared(); // Force is negative because it is applied on object 2
     r.normalize() * force // Normalize the vector and multiply by the force
@@ -45,7 +45,7 @@ mod tests {
     // Test attraction force vector function
     #[test]
     fn test_get_attraction_force_vector() {
-        let result = get_attraction_force_vector3(1.0, 1.0, na::Vector2::new(1.0, 1.0));
+        let result = get_attraction_force_vector2(1.0, 1.0, na::Vector2::new(1.0, 1.0));
         assert_eq!(result, na::Vector2::new(-2.359643613090756 * 10.0_f64.powi(-11), -2.359643613090756 * 10.0_f64.powi(-11)));
     }
 
